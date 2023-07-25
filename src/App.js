8cd7f6f3476697289331fd976974651896af31ee
Sumit0397 <sumitmajumdar16@gmail.com>
2023-07-25 21:22:02 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
@@ -5,6 +6,7 @@ import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <div>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
